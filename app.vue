62ddf3216083a618f65b4899ab724fa85a5a4d1b
Tougrel <lambrosagris@gmail.com>
2024-06-01 16:43:53 +0300

    initial commit

---
+++ app.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+useHead({
+	title: "ΒΡΩΜΙΑΡΑ ΧΗΝΑ",
+	link: [
+		{
+			rel: "icon",
+			href: "/favicon.svg",
+		},
+		{
+			rel: "mask-icon",
+			href: "/favicon.svg",
+			color: "#FFF",
+		},
+	],
+});
+</script>
+
+<template>
+	<h1 class="text-4xl font-bold text-red-400">ΠΡΟΣΟΧΗ</h1>
+	<p class="text-xl font-medium text-blue-400">Η ΠΑΠΙΑ ΕΙΝΑΙ ΒΡΩΜΙΑΡΑ</p>
+	<NuxtImg src="assets/goose.jpg" width="1024" class="rounded-lg" />
+</template>
+
+<style>
+html {
+	@apply flex flex-col items-center text-center justify-center h-screen bg-neutral-900;
+}
+</style>
